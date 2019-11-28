158a3b83f178a39960505cae1bb69aba58876e42
kent1121 <kent1121.sub@gmail.com>
2019-11-28 02:45:09 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Microposts</a>
+        
+        <button type="button" class="navbar-toggler" data-toggler="collapse" data-target="#navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="navbar-item"><a class="nav-link" href="#">Signup</a></li>
+                <li class="navbar-item"><a class="nav-link" href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
